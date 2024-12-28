b6a413938508b6810df9ea33e15190c9417ac2e9
kavos113 <kohei.hadano@gmail.com>
2024-12-28 10:25:59 +0900

    udp both

--- winsock/udp/both.cpp
+++ winsock/udp/both.cpp
@@ -27,6 +27,11 @@ int main(int argc, char** argv)
 
     std::thread t(recieve);
     t.join();
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::cout << "Sending value..." << std::endl;
+    sendvalue(argv[1]);
+    std::cout << "Done" << std::endl;
 }
 
 int sendvalue(char* ipaddr)
